819f62342c2d43b94a9a3e808fceb26b403e769d
puskar <puskr23@gmail.com>
2020-08-28 20:40:18 +0545

    first

---
+++ src/naruto1/Hinata.java
@@ -0,0 +1,9 @@
+package naruto1;
+
+public class Hinata {
+	int a=5;
+	int b=6;
+	int s=a+b;
+
+
+}
---
+++ src/naruto2/Sakura.java
@@ -0,0 +1,10 @@
+package naruto2;
+
+public class Sakura {
+	int a=0;
+	int b=9;
+	int c=a+b;
+
+	}
+
+

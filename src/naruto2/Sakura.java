38228e359b0a944f06c0be610e1861dd9cd4c603
puskar <puskr23@gmail.com>
2020-08-30 11:58:40 +0545

    changes to ithari branches

--- src/naruto2/Sakura.java
+++ src/naruto2/Sakura.java
@@ -7,6 +7,10 @@ public class Sakura {
 	public static void main(String[] args) {
 		System.out.println("this is just practice");
 		System.out.println("ithar");
+		System.out.println("This");
+		System.out.println("is");
+		System.out.println("itahri");
+		System.out.println("branch");
 	}
 
 	}

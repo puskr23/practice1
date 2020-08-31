38e195664bd5699a077970d0e185f5f51f1ed1c7
puskar <puskr23@gmail.com>
2020-08-31 09:14:48 +0545

    monday1

--- src/naruto2/Sakura.java
+++ src/naruto2/Sakura.java
@@ -6,6 +6,7 @@ public class Sakura {
 	int c=a+b;
 	public static void main(String[] args) {
 		System.out.println("this is master branch");
+		System.out.println("this is ithari branch");
 		
 	}
 	
